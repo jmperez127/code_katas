cc36eef5b3b6cada4efe8cb719496b8a569b50c7
theil <theil_27@hotmail.com>
2015-11-16 12:21:09 -0430

    test for bottom and half array binary search

--- KarateChop/src/BinaryChop.java
+++ KarateChop/src/BinaryChop.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryChop {
+    protected List<Integer> items;
+    private int iterationCount;
+
+    public BinaryChop() {
+        iterationCount = 0;
+        items = new ArrayList<Integer>();
+    }
+
+    public List<Integer> getItems() {
+        return items;
+    }
+
+    public void addValue(int value) {
+        items.add(value);
+    }
+
+    public int findPositionByValue(int value) {
+        if (items.get(0) == value) {
+            iterationCount++;
+            return 0;
+        }
+
+        int middle = (items.size() - 1) / 2;
+
+        if (items.get(middle) == value) {
+            iterationCount++;
+            return middle;
+        }
+
+        return -1;
+    }
+
+    public int getIterationCount() {
+        return iterationCount;
+    }
 }
--- KarateChop/test/BinaryChopTest.java
+++ KarateChop/test/BinaryChopTest.java
@@ -1,12 +1,55 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class BinaryChopTest {
+    BinaryChop binaryChop;
+    @Before
+    public void setUp(){
+        binaryChop = new BinaryChop();
+    }
 
     @Test
-    public void shouldHaveASortedArrayOfIntValuesToSearch() {
-        assertEquals("4 is equals to 4", 4, 4);
+    public void shouldHaveASortedArrayListOfIntValuesToSearch() {
+        assertEquals(binaryChop.getItems(), new ArrayList<Integer>());
     }
+
+    @Test
+    public void shouldGetFirstItemOnTheFirstTry() {
+        binaryChop.addValue(5);
+        binaryChop.addValue(15);
+        assertEquals(binaryChop.findPositionByValue(5), 0);
+        assertEquals(binaryChop.getIterationCount(), 1);
+    }
+
+    @Test
+    public void shouldGetMiddleItemOnTheFirstTry() {
+        binaryChop.addValue(5);
+        binaryChop.addValue(15);
+        binaryChop.addValue(54);
+        binaryChop.addValue(52);
+        binaryChop.addValue(12);
+        binaryChop.addValue(30);
+        binaryChop.addValue(43);
+        assertEquals(binaryChop.findPositionByValue(52), 3);
+        assertEquals(binaryChop.getIterationCount(), 1);
+    }
+
+//    @Test
+//    public void shouldSearchForSpecifiedValue() {
+//        binaryChop.addValue(5);
+//        binaryChop.addValue(15);
+//        binaryChop.addValue(54);
+//        binaryChop.addValue(52);
+//        binaryChop.addValue(12);
+//        binaryChop.addValue(30);
+//        binaryChop.addValue(43);
+//        assertEquals(binaryChop.findPositionByValue(15), 1);
+//    }
+
 
 }
