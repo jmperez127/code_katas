e397bc005a4fb686f1d30ec5314d0d6fd9d8fddd
theil <theil_27@hotmail.com>
2015-11-19 05:26:05 -0430

    Add Messy and ugly iterative implementation.

--- KarateChop/src/BinaryChop.java
+++ KarateChop/src/BinaryChop.java
@@ -1,2 +1,51 @@
 public class BinaryChop {
+// in the first pass it determines whether the required value is in the top or the bottom
+// half of the list of values. In the second pass in considers only this half, again dividing it
+// in to two. It stops when it finds the value it is looking for, or when it runs out of array to search.
+// Binary searches are a favorite of CS lecturers.
+
+    public int chop(int value, int[] array) {
+        int bottomIndex = 0;
+        int topIndex = (array.length - 1);
+        int midIndex = topIndex / 2;
+
+
+
+        if(value > array[topIndex])
+            return -1;
+
+        if(value < array[bottomIndex])
+            return -1;
+
+        if(array.length == 1)
+            return 0;
+
+
+        while (bottomIndex < topIndex) {
+
+            if (value == array[bottomIndex])
+                return bottomIndex;
+
+            if (value == array[topIndex])
+                return topIndex;
+
+            if (value == array[midIndex])
+                return midIndex;
+
+
+            if (value < array[midIndex]) {
+                bottomIndex++;
+                topIndex = midIndex;
+                midIndex = ((topIndex / 2)+ bottomIndex);
+            } else if(value > array[midIndex]) {
+                bottomIndex = midIndex;
+                midIndex = ((topIndex - bottomIndex)/ 2) + midIndex;
+            }
+            else
+                return midIndex;
+        }
+
+
+        return -1;
+    }
 }
